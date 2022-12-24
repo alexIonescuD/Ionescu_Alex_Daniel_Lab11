1b9a821faffe3803f7da72502ad4830d2e90c184
alexIonescuD <dionescu90@yahoo.com>
2022-12-24 20:02:13 +0200

    Add project files.

---
+++ Ionescu_Alex_Daniel_Lab11/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Ionescu_Alex_Daniel_Lab11.Data;
+namespace Ionescu_Alex_Daniel_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+    }
+}
---
+++ Ionescu_Alex_Daniel_Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ionescu_Alex_Daniel_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
